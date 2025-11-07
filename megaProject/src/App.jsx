df8cac93317d46549bae5bbabd1bd78c47e1bbeb
ramkachhwaha <ramkachhwaha062@gmail.com>
2025-11-07 18:49:41 +0530

    first commit

---
+++ megaProject/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  // MegaProject
+
+  console.log(import.meta.env.VITE_ENPOINT_URL);
+  return (
+    <>
+
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quod at ratione ipsum!</p>
+    </>
+  )
+}
+
+export default App
